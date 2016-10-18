3de53c33a6a912f77435fc6a87922ccd49d91e9c
Andy Van Slaars <avanslaars@urbn.com>
2016-10-18 13:44:24 -0400

    Added ToogleTodo UpdateTodo and FindById with tests

--- src/Helpers.js
+++ src/Helpers.js
@@ -1,3 +1,16 @@
 export const AddItem = (list, item) => list.concat(item)
 
 export const GenerateId = () => Math.floor(Math.random()*100000)
+
+export const ToggleTodo = (todo) => Object.assign({}, todo, {isComplete: !todo.isComplete})
+
+export const UpdateTodo = (list, updated) => {
+    const updatedIndex = list.findIndex(item => item.id === updated.id)
+    return [
+        ...list.slice(0, updatedIndex),
+        updated,
+        ...list.slice(updatedIndex+1)
+    ]
+}
+
+export const FindById = (list, id) => list.find(item => item.id === id)
--- src/Helpers.test.js
+++ src/Helpers.test.js
@@ -1,4 +1,4 @@
-import {AddItem} from './Helpers'
+import {AddItem, ToggleTodo, UpdateTodo, FindById} from './Helpers'
 
 test('AddItem Returns An Updated List', () => {
   const Expected = [1,2,3]
@@ -13,4 +13,51 @@ test('AddItem Returns An Updated List', () => {
   const result = AddItem(list, 3)
   // Verifying immutable nature of the concatenated array
   expect(result).not.toBe(list)
+})
+
+test('toggleTodo marks an incomplete item complete', () => {
+    const input = {id:1, name:'Sample todo', isComplete:false}
+    const expected = {id:1, name:'Sample todo', isComplete:true}
+    const result = ToggleTodo(input)
+    expect(result).toEqual(expected)
+})
+
+test('toggleTodo marks a complete item incomplete', () => {
+    const input = {id:1, name:'Sample todo', isComplete:true}
+    const expected = {id:1, name:'Sample todo', isComplete:false}
+    const result = ToggleTodo(input)
+    expect(result).toEqual(expected)
+})
+
+test('FindById returns the todo with a matching id', () => {
+    const todos = [
+        {id:1, name:'Sample one', isComplete:false},
+        {id:2, name:'Sample two', isComplete:false},
+        {id:3, name:'Sample three', isComplete:false}
+    ]
+
+    const expected = {id:2, name:'Sample two', isComplete:false}
+    const result = FindById(todos, 2)
+    expect(result).toEqual(expected)
+})
+
+test('UpdateTodo returns a new list with the specified todo replaced by id', () => {
+    const todos = [
+        {id:1, name:'Sample one', isComplete:false},
+        {id:2, name:'Sample two', isComplete:false},
+        {id:3, name:'Sample three', isComplete:false}
+    ]
+    const updated = {id:2, name:'Sample two', isComplete:true}
+
+    const expected = [
+        {id:1, name:'Sample one', isComplete:false},
+        {id:2, name:'Sample two', isComplete:true},
+        {id:3, name:'Sample three', isComplete:false}
+    ]
+
+    const result = UpdateTodo(todos, updated)
+
+    expect(result).toEqual(expected)
+
+
 })
